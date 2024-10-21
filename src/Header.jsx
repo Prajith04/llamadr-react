1ea4171ff186168a4bbe6640e7c5366bb607fd4b
Prajith21 <prajith_22aib21@kgkite.ac.in>
2024-10-21 22:16:57 +0530

    added header

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css';
+function Header() {
+  return (
+    <header>
+      <div className='header'>
+      <h1 className='title'>LlamaDr</h1>
+      <nav>
+      <h1>Home</h1>
+      <h1>About</h1>
+      <h1>Contact</h1>
+      </nav>
+      </div>
+    </header>
+  );
+}
+export default Header;
